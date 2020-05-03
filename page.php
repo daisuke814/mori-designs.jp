a822c5c095d47e5851004d977e9c0473ef1ec83b
Daisuke Sakamoto <41839638+daisuke814@users.noreply.github.com>
2020-05-04 01:38:33 +0900

    no message

--- page.php
+++ page.php
@@ -2,18 +2,16 @@
 
 <?php get_header() ?>
 
-<main>
+<main id="page">
 
     <div class="wrapper">
         <?php
             if ( have_posts() ) :
                 while ( have_posts() ) : the_post();
                     ?>
-                    <h2>
-                        <?php the_time('Y年n月j日'); ?>&nbsp;&nbsp;
-                        <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
-                    </h2>
-                    <hr>
+                    <h2><?php the_title(); ?></h2>
+                    <?php the_time('Y年n月j日'); ?>
+                    <?php the_content(); ?>
                 <?php
                 endwhile;
             endif;
